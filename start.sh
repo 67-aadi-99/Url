f6381d43ce0a9f941f196211dfb0080348b45689
67-aadi-99 <92543854+67-aadi-99@users.noreply.github.com>
2023-07-29 12:46:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/Jakeedot/URLShortner.git /URLShortener
-cd /URLShortener
+git clone https://github.com/67-aadi-99/URLShortner.git /67-aadi-99 /67-aadi-99
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
